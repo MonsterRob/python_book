a6a46b393696aeca66940004ffe24a3509894915
MonsterRob <wmwwzhao@163.com>
2017-11-08 23:33:03 +0800

    add

---
+++ fluent_python/3.1.py
@@ -0,0 +1,11 @@
+# encoding=utf-8
+
+d = dict(one=1, two=2)
+d1 = {'one': 1, "two": 2}
+d2 = dict()
+d2['one'] = 1
+d2['two'] = 2
+# [(key,value), (key, value)]
+
+my_dict = {}.setdefault().append(1)
+print(my_dict[1])
---
+++ fluent_python/5.6.py
@@ -0,0 +1,8 @@
+# encoding=utf-8
+
+
+def f(name=10):
+    print(name)
+
+
+print(f.__defaults__)
