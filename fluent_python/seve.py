e6a247e37a029a510023e9ad0fdd12d19c5dac3c
wangzhaozhou <wangzhaozhu@peiwo.cn>
2017-11-09 14:43:30 +0800

    add

---
+++ fluent_python/seve.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+name = 10
+
+print('defi outer name')
+
+
+# 导入时运行
+
+class A:
+    print('defi name')
+    name = 20
+
+    # 导入时运行
+    def say(self):
+        # 调用时运行
+        self.name = 10
+
+
+def f():
+    name = 10
+    print(name)
