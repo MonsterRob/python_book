084a017703d4a040ef76ef07c448e22a26181e18
wangzhaozhou <wangzhaozhu@peiwo.cn>
2017-11-14 12:27:44 +0800

    add

--- fluent_python/a20171113.py
+++ fluent_python/a20171113.py
@@ -22,4 +22,49 @@ class Item:
             raise ValueError('value must be > 0')
 
 
-item = Item('name', 0, 100)
+class A(object):
+    def __init__(self, price, weight):
+        self.price = price
+        self.weigth = weight
+
+    def get_price(self):
+        return self.__price
+
+    def set_price(self, value):
+        if value < 0:
+            raise ValueError('Value b=nust be > 0')
+        self.__price = value
+
+    price = property(fget=get_price, fset=set_price)
+
+
+class B(object):
+    name = 'xxx'
+
+    def __init__(self, name):
+        self.name = name
+
+
+def quantity(storage_name):
+    print('defination qty')
+
+    def qty_get(instance):
+        return instance.__dict__[storage_name]
+
+    def qty_set(instance, value):
+        if value < 0:
+            raise ValueError('value must be > 0')
+        instance.__dict__[storage_name] = value
+
+    return property(fget=qty_get, fset=qty_set)
+
+
+class C(object):
+    price = quantity('price')
+
+    def __init__(self, price):
+        self.price = price
+
+
+c = C(price=10)
+
